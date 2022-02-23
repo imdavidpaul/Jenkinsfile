2a422f81103a556a69d1a3ffad92ddee031f116a
imdavidpaul <dannydavid.paul10@gmail.com>
2022-02-23 07:18:03 +0000

    Added groovy

---
+++ vars/math.groovy
@@ -0,0 +1,12 @@
+def sum(a,b){
+ echo "The sum of ${a} and ${b} is ${a+b}"
+}
+def sub(a,b){
+ echo "The sub of ${a} and ${b} is ${a-b}"
+}
+def mult(a,b) {
+ echo "The multiply of ${a} and ${b} is ${a*b}"
+}
+def divi(a,b){
+ echo "The division of ${a} and ${b} is ${a/b}"
+}
